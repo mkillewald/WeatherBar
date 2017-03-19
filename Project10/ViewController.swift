ef9132c92c421f9f00f2cc229c392eb4816fc484
Mike Killewald <mkillewald@yahoo.com>
2017-03-19 10:34:15 -0400

    Initial Commit

---
+++ Project10/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by k1ds3ns4t10n on 3/19/17.
+//  Copyright © 2017 Gameaholix. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
